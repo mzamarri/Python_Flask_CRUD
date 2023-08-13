517821aefbd1ecf851f8ab7ff0a786c79bddf54e
mzamarri <132020928+mzamarri@users.noreply.github.com>
2023-08-13 16:00:04 -0700

    Update sentiment_analysis.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -7,6 +7,11 @@ def sentiment_analyzer(text_to_analyze):
     myobj = {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    label = formatted_response['documentSentiment']['label']
-    score = formatted_response['documentSentiment']['score']
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    elif response.status_code == 500:      
+        label = None
+        score = None
     return {'label': label, 'score': score}
+    
